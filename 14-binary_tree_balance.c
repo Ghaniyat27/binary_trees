0e3c172213d53e47f56f96ebb5fed79c3996c2b8
Ghaniyat <ganiyat4real1@gmail.com>
2024-03-02 04:05:39 -0800

    Balance Factor Task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,46 +1,41 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_b - Determine's The Height of a Binary tree for a bal tree
- * @tree: tree to Traverse
- * Return: The Height
+ * binary_tree_balance - Measures' the Bal Factor of a Binary tree
+ * @tree:Ppointer to the Root Node of Tree to Measure the Bal Factor
+ *
+ * Return: The Balance Factor
+ *         0 if tree is NULL
  */
-
-size_t binary_tree_height_b(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t l = 0;
-	size_t r = 0;
+	int height_l, height_r;
 
-	if (tree == NULL)
-	{
+	if (!tree)
 		return (0);
-	}
-	else
-	{
-		if (tree)
-		{
-			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
-			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
-		}
-		return ((l > r) ? l : r);
-	}
+
+	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
+	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
+
+	return (height_l - height_r);
 }
 
 /**
- * binary_tree_balance - Determine's bal. Factor of  Binary tree
- * @tree: tree to traverse
- * Return: Balanced Factor
+ * binary_tree_height -Mmeasure's the Height of a Binary tree
+ * @tree: The tree to Measure the Height
+ *
+ * Return: Height of the tree
+ *         0 if tree is NULL
  */
-
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int right = 0, left = 0, total = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-	if (tree)
-	{
-		left = ((int)binary_tree_height_b(tree->left));
-		right = ((int)binary_tree_height_b(tree->right));
-		total = left - right;
-	}
-	return (total);
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
